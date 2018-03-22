c612f504d8fdcea15b06653bd8fc0c2c74d17472
Paulo Lima <paulobruny@gmail.com>
2018-03-21 22:36:05 -0300

    Add script tag and first ever JavaScript instructions

---
+++ js/app.js
@@ -0,0 +1,13 @@
+//First Program in JavaScript
+
+console.time('Init');
+  console.log("Hello World!");
+  console.log("This is a test");
+  console.log(123);
+  console.log(true);
+  console.log([1, 2, 3, 4]);
+  console.log({a:1, b:2});
+  var test = 'Hello Dude';
+  console.log(test);
+  console.table({a:1, b:2});
+console.timeEnd('finish ma friend');
