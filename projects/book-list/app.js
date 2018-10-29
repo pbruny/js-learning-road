7b0b75a0594a5a12e4169c1e904acf80c13d193b
Paulo Lima <paulobruny@gmail.com>
2018-10-29 13:14:33 -0300

    Validation and alerts to error and success

--- projects/book-list/app.js
+++ projects/book-list/app.js
@@ -1,12 +1,12 @@
-function Book(title, author, isbn){
+function Book(title, author, isbn) {
     this.title = title;
     this.author = author;
     this.isbn = isbn;
 }
 
-function UI(){}
+function UI() { }
 
-UI.prototype.addBookToList = function(book){
+UI.prototype.addBookToList = function (book) {
     const list = document.querySelector('#book-list');
     const row = document.createElement('tr');
 
@@ -16,20 +16,32 @@ UI.prototype.addBookToList = function(book){
         <td>${book.isbn}</td>
         <td><a href="#" class="delete">X</a></td>
     `;
-    list.appendChild(row);    
-    
+    list.appendChild(row);
+
 }
 
-UI.prototype.clearFields = function(){
+UI.prototype.clearFields = function () {
     const title = document.querySelector('#title').value = '';
     const author = document.querySelector('#author').value = '';
     const isbn = document.querySelector('#isbn').value = '';
 }
 
+UI.prototype.showAlert = function (message, className){
+    const div = document.createElement('div');
+    div.className = `alert ${className}`;
+    div.appendChild(document.createTextNode(message));
+    const container = document.querySelector('.container');
+    const form = document.querySelector('.book-form');
 
-const bookForm = document.querySelector('.book-form');
+    container.insertBefore(div, form);
 
-bookForm.addEventListener('submit', function(e){
+    setTimeout(function(){
+        document.querySelector('.alert').remove();
+    }, 3000);
+}
+
+
+document.querySelector('.book-form').addEventListener('submit', function (e) {
     const title = document.querySelector('#title').value;
     const author = document.querySelector('#author').value;
     const isbn = document.querySelector('#isbn').value;
@@ -37,8 +49,15 @@ bookForm.addEventListener('submit', function(e){
     const book = new Book(title, author, isbn);
 
     const ui = new UI();
-    ui.addBookToList(book);
-    ui.clearFields();
-    
+
+    if(title === '' || author === '' || isbn === ''){
+        ui.showAlert(`Please, fill in all the fields`, 'error');
+    }else{
+        ui.addBookToList(book);
+        ui.showAlert(`Book successfully added!`, 'success')
+        ui.clearFields();
+    }
+
+
     e.preventDefault();
 })
