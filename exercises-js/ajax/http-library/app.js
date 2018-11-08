91ff291a9e72f82892cf1e41e2eebd8c6aa6ff3e
Paulo Lima <paulobruny@gmail.com>
2018-11-08 00:45:22 -0300

    Version 3 of http library using async and await

--- exercises-js/ajax/http-library/app.js
+++ exercises-js/ajax/http-library/app.js
@@ -1,9 +1,9 @@
 const http = new EasyHTTP;
 
 //Get Users
-// http.get('https://jsonplaceholder.typicode.com/users')
-// .then(data => console.log(data))
-// .catch(err => console.log(err))
+/* http.get('https://jsonplaceholder.typicode.com/users')
+.then(data => console.log(data))
+.catch(err => console.log(err)) */
 
 const data = {
     name: 'John Doe',
@@ -12,14 +12,14 @@ const data = {
 }
 
 //Create user
-// http.post('https://jsonplaceholder.typicode.com/users', data)
-// .then(data => console.log(data))
-// .catch(err => console.log(err))
+/* http.post('https://jsonplaceholder.typicode.com/users', data)
+.then(data => console.log(data))
+.catch(err => console.log(err)) */
 
 //Update user
-// http.put('https://jsonplaceholder.typicode.com/users/3', data)
-// .then(data => console.log(data))
-// .catch(err => console.log(err))
+/* http.put('https://jsonplaceholder.typicode.com/users/3', data)
+.then(data => console.log(data))
+.catch(err => console.log(err)) */
 
 //Delete user
 http.delete('https://jsonplaceholder.typicode.com/users/3')
---
+++ exercises-js/ajax/http-library/easyhttp3.js
@@ -0,0 +1,46 @@
+class EasyHTTP {
+    //http get request
+    async get(url){
+        const response = await fetch(url);
+        const resData = await response.json();
+        return resData;
+    }
+
+    //http post request
+    async post(url, data){
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+             body: JSON.stringify(data)
+        })
+            const resData = await response.json();
+            return resData;
+    }
+
+    //http put request
+    async put(url, data){
+        const response = await fetch(url, {
+            method: 'PUT',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        const resData = await response.json();
+        return resData;
+    }
+
+    //http delete request
+    async delete(url){
+        const response = fetch(url, {
+            method: 'DELETE',
+            headers: {
+                'Content-type': 'application/json'
+            },
+        })
+        const resData = await 'Resource Deleted!';
+        return resData;
+    }
+}
