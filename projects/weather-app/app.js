006c753de6944dca904517ddd8629904557a7d52
Paulo Lima <paulobruny@gmail.com>
2018-11-19 14:28:08 -0300

    Filling the ui with hard coded information

--- projects/weather-app/app.js
+++ projects/weather-app/app.js
@@ -1,15 +1,12 @@
 const weather = new Weather('Boston', 'MA');
 const ui = new UI();
 
-weather.changeLocation('San Francisco', 'CA');
-
 document.addEventListener('DOMContentLoaded', getWeather);
 
 function getWeather(){
     weather.getWeather()
     .then(results => {
-        
-        
+        ui.fillUI(results);
     })
     .catch(err => console.log(err))
 }
---
+++ projects/weather-app/ui.js
@@ -0,0 +1,25 @@
+class UI {
+    constructor(){
+        this.location = document.querySelector('#w-location');
+        this.description = document.querySelector('#w-desc');
+        this.string = document.querySelector('#w-string');
+        this.icon = document.querySelector('#w-icon');
+        this.details = document.querySelector('#w-details');
+        this.humidity = document.querySelector('#w-humidity');
+        this.dewpoint = document.querySelector('#w-dewpoint');
+        this.feelsLike = document.querySelector('#w-feels-like');
+        this.wind = document.querySelector('#w-wind');
+    }
+
+    fillUI(weather){
+        this.location.textContent = weather.display_location.full;
+        this.description.textContent = weather.weather;
+        this.string.textContent = weather.temperature_string;
+        this.icon.setAttribute('src', weather.icon_url);
+        this.humidity.textContent = `Relative humidity: ${weather.relative_humidity}`;
+        this.dewpoint.textContent = `Dewpoint: ${weather.dewpoint_string}`
+        this.feelsLike.textContent = `Feels like: ${weather.feelslike_string}`;
+        this.wind.textContent = `Wind:  ${weather.wind_string} ${weather.wind_mph}mph ${weather.wind_dir}`;
+
+    }
+}
