5b9ec484a0900163330addd1e75aa2329334ff5f
Paulo Lima <paulobruny@gmail.com>
2018-11-26 19:50:22 -0300

    Hard coded profile data to test

---
+++ projects/profile-scroller/app.js
@@ -0,0 +1,43 @@
+const data = [
+    {
+        name: 'John Doe',
+        age: 32,
+        gender: 'male',
+        lookingfor: 'female',
+        location: 'Boston MA',
+        image: 'https://randomuser.me/api/portraits/men/89.jpg'
+    },
+    {
+        name: 'Marlon Brando',
+        age: 22,
+        gender: 'male',
+        lookingfor: 'female',
+        location: 'New York NY',
+        image: 'https://randomuser.me/api/portraits/men/26.jpg'
+    },
+    {
+        name: 'Mary Jane',
+        age: 25,
+        gender: 'female',
+        lookingfor: 'male',
+        location: 'Boston MA',
+        image: 'https://randomuser.me/api/portraits/women/90.jpg'
+    },
+    {
+        name: 'Jenny Smith',
+        age: 20,
+        gender: 'female',
+        lookingfor: 'female',
+        location: 'San Francisco CA',
+        image: 'https://randomuser.me/api/portraits/women/21.jpg'
+    },
+    {
+        name: 'Januario Jose',
+        age: 37,
+        gender: 'male',
+        lookingfor: 'male',
+        location: 'Miami FL',
+        image: 'https://randomuser.me/api/portraits/men/24.jpg'
+    }
+]
+
